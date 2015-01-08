cfd125962dbfa26ece3927cf8201b016e23a09aa
vadimhanson <hanson@i.ua>
2015-01-08 11:02:21 +0200

    Create hu.php

---
+++ payu/hu.php
@@ -0,0 +1,20 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{payu}prestashop>payu_44d04613dc6cee7844082b936b831b74'] = 'PayU credit card payment';
+$_MODULE['<{payu}prestashop>payu_ce9b3b07848742e71ff17917ab009180'] = 'Payment PayU with';
+$_MODULE['<{payu}prestashop>payu_74d197059e342182de967754a6c78d77'] = 'Do you want to remove?';
+$_MODULE['<{payu}prestashop>payu_5dd532f0a63d89c5af0243b74732f63c'] = 'Settings';
+$_MODULE['<{payu}prestashop>payu_3121e1d3c5c363038c724894c3d49ceb'] = 'Please check PayU account information';
+$_MODULE['<{payu}prestashop>payu_896890bfb700eac98300d639ca970f2b'] = 'Dealer Code:';
+$_MODULE['<{payu}prestashop>payu_952bf87c967660b7bbd4e1eb08cefc92'] = 'Encryption key:';
+$_MODULE['<{payu}prestashop>payu_36c6e62ae291332012bbc625bdaa6fa6'] = 'system link';
+$_MODULE['<{payu}prestashop>payu_adbdf61c7210f15c6c8acfda0b9c5bcf'] = 'FX:';
+$_MODULE['<{payu}prestashop>payu_ec3028a12402ab7f43962a6f3a667b6e'] = 'debug mode';
+$_MODULE['<{payu}prestashop>payu_816a2ce153678d71d88ec40344ffe1b4'] = 'Back ref url';
+$_MODULE['<{payu}prestashop>payu_38c5840ba6ad7dc4dbfc6534d43bab36'] = 'Family Site language:';
+$_MODULE['<{payu}prestashop>payu_b17f3f4dcf653a5776792498a9b44d6a'] = 'update settings';
+$_MODULE['<{payu}prestashop>payu_9aa5e557d3f1ab9a71912d9f0bc59b3c'] = 'This module allows you to pay by credit card.';
+$_MODULE['<{payu}prestashop>payu_bfa43217dfe8261ee7cb040339085677'] = 'necessary account information';
+$_MODULE['<{payu}prestashop>payu_c888438d14855d7d96a2724ee9c306bd'] = 'Update Settings';
