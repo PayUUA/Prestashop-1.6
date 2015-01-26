29a8be6b918d3348d9a61ff3b8c4d29b38fe1150
vadimhanson <hanson@i.ua>
2015-01-26 12:14:16 +0200

    Create defaults.php

---
+++ payu/sdk/examples/defaults.php
@@ -0,0 +1,12 @@
+<?php
+
+$defaults = array(
+    'BASE_URL' => "https://secure.payu.ua/", //please add TRAILING SLASH
+    'LU_URL' => "order/lu.php", //relative to BASE_URL
+    'ALU_URL' => "order/alu.php", //relative to BASE_URL
+    'IDN_URL' => "order/idn.php", //relative to BASE_URL
+    'IRN_URL' => "order/irn.php", //relative to BASE_URL
+    'IOS_URL' => "order/ios.php", //relative to BASE_URL
+);
+
+?>
